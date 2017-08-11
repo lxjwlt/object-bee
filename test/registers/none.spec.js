75b29abeef6b3701f66743b15a590f8bea8e5626
lxjwlt <lxjwlt.c@gmail.com>
2017-08-11 22:45:21 +0800

    noop test

---
+++ test/registers/none.spec.js
@@ -0,0 +1,136 @@
+'use strict';
+
+const {check} = require('../util');
+const bee = require('../../src/index');
+
+describe('[register] noop', () => {
+
+    it('normal', () => {
+        let ori = {
+            foo: 'bar'
+        };
+
+        let beeOptions = {
+            privacy: bee.noop()
+        };
+
+        check(ori, beeOptions, {
+            foo: 'bar'
+        });
+    });
+
+    it('in deep', () => {
+        let ori = {
+            node: {
+                info: {
+                    data: {
+                        name: 'foo'
+                    }
+                }
+            }
+        };
+
+        let beeOptions = {
+            node: {
+                info: {
+                    data: {
+                        name: bee.noop()
+                    }
+                }
+            }
+        };
+
+        check(ori, beeOptions, {
+            node: {
+                info: {
+                    data: {
+                        name: 'foo'
+                    }
+                }
+            }
+        });
+    });
+
+    it('unknown key', () => {
+        let ori = {
+            foo: 1
+        };
+
+        let beeOptions = {
+            unknownKey: bee.noop()
+        };
+
+        check(ori, beeOptions, {
+            foo: 1
+        });
+    });
+
+    it('multi apply', () => {
+        let ori = {
+            name: []
+        };
+
+        let beeOptions = {
+            name: [bee.noop(), bee.noop(), bee.noop()]
+        };
+
+        check(ori, beeOptions, {
+            name: []
+        });
+    });
+
+    it('mixed apply', () => {
+        let ori = {
+            name: []
+        };
+
+        let beeOptions = {
+            name: [bee.rename('foo'), bee.noop()]
+        };
+
+        check(ori, beeOptions, {
+            foo: []
+        });
+    });
+
+    it('chaining call', () => {
+        let ori = {
+            num: 1
+        };
+
+        let beeOptions = {
+            num: bee.noop().noop()
+        };
+
+        check(ori, beeOptions, {
+            num: 1
+        });
+    });
+
+    it('mixed chaining call', () => {
+        let ori = {
+            foo: 1
+        };
+
+        let beeOptions = {
+            foo: bee.noop().rename('b')
+        };
+
+        check(ori, beeOptions, {
+            b: 1
+        });
+    });
+
+    it('removed key', () => {
+        let ori = {
+            num: 1
+        };
+
+        let beeOptions = {
+            num: [bee.remove(), bee.noop()]
+        };
+
+        check(ori, beeOptions, {});
+    });
+
+});
