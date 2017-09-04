720a27c4d99c2d277ebf975d2ba9de68e1e5cca5
lxjwlt <lxjwlt.c@gmail.com>
2017-09-04 23:15:26 +0800

    build min and develop files

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,42 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
-        library: 'bee',
-        libraryTarget: 'umd'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [['env', {
-                            targets: {
-                                'ie': 9
-                            }
-                        }]]
+module.exports = function (env) {
+    let plugins = [];
+
+    if (env === 'min') {
+        plugins.push(
+            new webpack.optimize.UglifyJsPlugin({
+                comments: false
+            })
+        );
+    }
+
+    return {
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: env === 'min' ? 'index.min.js' : 'index.js',
+            library: 'bee',
+            libraryTarget: 'umd'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [['env', {
+                                targets: {
+                                    'ie': 9
+                                }
+                            }]]
+                        }
                     }
                 }
-            }
-        ]
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin()
-    ]
+            ]
+        },
+        plugins: plugins
+    };
 };
