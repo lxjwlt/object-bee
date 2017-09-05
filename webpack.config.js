1f08063e50ba73ac032da9ba6af58de9073d222b
lxjwlt <lxjwlt.c@gmail.com>
2017-09-06 00:01:37 +0800

    add banner for bundled file

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
 const path = require('path');
 const webpack = require('webpack');
+const pkg = require('./package.json');
 
 module.exports = function (env) {
-    let plugins = [];
+    let plugins = [
+        new webpack.BannerPlugin({
+            banner: [
+                `${pkg.name} v${pkg.version}`,
+                `(c) 2017-present ${pkg.author}`,
+                `Released under the ${pkg.license} license`,
+                `${pkg.repository.url}`,
+            ].join('\n'),
+            entryOnly: true
+        })
+    ];
 
     if (env === 'min') {
         plugins.push(
             new webpack.optimize.UglifyJsPlugin({
-                comments: false
+                comments: new RegExp(`${pkg.name}\\sv\\d+\\.\\d+\\.\\d+`)
             })
         );
     }
